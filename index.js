c9eaa63b3a82e08148bbb619df2d1edf4dc9a131
Sanni Opeyemi <sanniopeyemiolayinka@gmail.com>
2024-12-20 16:54:01 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db"); 
+const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const qRoutes = require("./routes/questionRoutes");
 const gRoutes = require("./routes/gameRoutes");
-const dRoutes = require("./routes/dashboard");
 const pRoutes = require("./routes/profileRoutes");
 const app = express();
 
+// Connect to the database
 connectDB();
 
-// app.use(cors());
-// app.use(express.json()); 
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/auth", authRoutes); 
-app.use("/api/questions", qRoutes); 
-app.use("/api/games", gRoutes); 
-app.use("/api/games", dRoutes); 
-app.use("/api/profile", pRoutes); 
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/questions", qRoutes);
+app.use("/api/games", gRoutes);
+app.use("/api/games", dRoutes);
+app.use("/api/profile", pRoutes);
 app.use('/uploads', express.static('uploads'));
 
-
-app.use("/", (req, res) => {
+// Fallback Route
+app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from Vercel!" });
 });
 
