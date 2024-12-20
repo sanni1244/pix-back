3221b5c854231d640082aba164fe409d4f44b1aa
Sanni Opeyemi <sanniopeyemiolayinka@gmail.com>
2024-12-20 15:57:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db"); 
-const authRoutes = require("./routes/authRoutes");
+// const authRoutes = require("./routes/authRoutes");
 // const qRoutes = require("./routes/questionRoutes");
 // const gRoutes = require("./routes/gameRoutes");
 // const dRoutes = require("./routes/dashboard");
 // const pRoutes = require("./routes/profileRoutes");
 
 const app = express();
-// app.use(express.json()); 
+app.use(express.json()); 
 
 dotenv.config();
 connectDB();
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from Vercel!" });
 });
 
-app.use("/api/auth", authRoutes); 
+// app.use("/api/auth", authRoutes); 
 // app.use("/api/questions", qRoutes); 
 // app.use("/api/games", gRoutes); 
 // app.use("/api/games", dRoutes); 
