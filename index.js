c43bce83cb4ffd585bee47252fbb2cc77907fde7
Sanni Opeyemi <sanniopeyemiolayinka@gmail.com>
2024-12-20 15:41:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-// const qRoutes = require("./routes/questionRoutes");
-// const gRoutes = require("./routes/gameRoutes");
-// const dRoutes = require("./routes/dashboard");
-// const pRoutes = require("./routes/profileRoutes");
-const port = 5000;
-
-dotenv.config();
-connectDB();
 
 const app = express();
- 
-app.use(cors());
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
-  res.status(200).json({ message: 'Login successful' });
+
+// Simple route
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from Vercel!" });
 });
 
-app.use(express.json());
-
-// API routes
-app.use("/api/auth", authRoutes);
-// app.use("/api/questions", qRoutes);
-// app.use("/api/games", [gRoutes, dRoutes]); 
-// app.use("/api/profile", pRoutes);
-
-// Static file serving for uploads
-// app.use('/uploads', express.static('uploads'));
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Export the app for Vercel
+module.exports = app;
